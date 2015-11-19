c57e25510871b77dd8f62d1a4198aa24e8c28bb4
yjwang <wyjss08@gmail.com>
2015-11-19 11:28:35 +0800

    add trace to CUT_DOWN_ROD

--- DP/dp2.c
+++ DP/dp2.c
@@ -7,8 +7,8 @@
 
 #include<stdio.h>
 int p[] = {0,1,5,8,9,10,17,17,20,24,30};
-int CUT_DOWN_ROD(int n, int p[]){
-	int i,j,s,q;
+int CUT_DOWN_ROD(int n, int p[], int trace[]){
+	int i,j,s,q,k;
 	int r[n+1];
 	r[0] = 0;
 	for(i=1;i<=n;++i){
@@ -16,20 +16,30 @@ int CUT_DOWN_ROD(int n, int p[]){
 	}
 	for(j=1;j<=n;++j){
 		q = -1;
+		k = 0;
 		for(i=1;i<=j;++i){
 			s = p[i] + r[j-i];
 			if(q<s){
 				q = s;
+				k = i;
 			}
 		}
 		r[j] = q;
+		trace[j] = k;
 	}
 	return r[n];
 }
 int main(){
-	int n;
+	int n,s,j;
 	printf("pls enter number: \n");
 	scanf("%d",&n);
-	printf("Max_Value is %d",CUT_DOWN_ROD(n,p));
+	int trace[n+1];
+	s = CUT_DOWN_ROD(n,p,trace);
+	printf("Max_Value is %d\n",s);	
+	j = n;
+	while(j != 0){
+		printf("%d ",trace[j]);
+		j = j - trace[j];
+	}
 	return 0;
 }
